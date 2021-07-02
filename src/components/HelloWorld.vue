d9f0cfdb66b89acca79bb1aa86c514b1ab35d2a6
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-07-02 09:43:53 +0200

    init commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,8 +39,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
@@ -53,6 +52,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: $red;
 }
 </style>
