c2cb870a73c8ecb419f44a84727e35aa138bca5c
Antoine Puech <anto.puech@gmail.com>
2021-07-05 18:07:02 +0200

    creating footer and add scss file for footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navbar></Navbar>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer.vue"
 
 export default {
     components: {
-        Navbar
+        Navbar,
+        Footer
     }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <img src="../assets/logos/logo_blanc.svg" alt="logo iimpact" id="logo" />
+    <div class="footer_nav">
+      <router-link to="home">Accueil</router-link>
+      <router-link to="membres">Membres</router-link>
+      <router-link to="evenements">Évènements</router-link>
+      <router-link to="blog">Blog</router-link>
+    </div>
+    <div class="last_events">
+      <p id="events">Derniers évènements</p>
+      <p>lorem ipsum</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
